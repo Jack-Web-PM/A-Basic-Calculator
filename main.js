138a9a980c08bb57ebc1391d1ba7b4ef7ecd8a31
Jack-Web-PM <jack.chou24@student.cart.org>
2025-01-29 13:33:35 -0800

    should finished the last one first

---
+++ main.js
@@ -0,0 +1,11 @@
+// document.querySelector("html").addEventListener("click", () => {
+//     alert("this is wrong")
+// });
+    
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function inputNumber() {
+    const number = prompt("please input some numbers.");
+    
+}
