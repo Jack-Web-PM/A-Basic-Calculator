e9f4c81570b99143f2cdfe6aef62add1ea5df1af
Jack-Web-PM <jack.chou24@student.cart.org>
2025-01-30 13:57:45 -0800

    update

--- main.js
+++ main.js
@@ -4,8 +4,14 @@
     
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
+let input = document.querySelector("input");
 
 function inputNumber() {
     const number = prompt("please input some numbers.");
+    localStorage.setItem("1", number);
+    myHeading.textContent = '1, ${number}';
+}
+
+function inputText () {
     
 }
