7725b119e9238a43d99f7e980984e121113808c7
Jack-Web-PM <jack.chou24@student.cart.org>
2025-01-30 23:04:10 -0800

    update

--- main.js
+++ main.js
@@ -1,16 +1,68 @@
-let x = num1;
-let y = num2;
-let (x + y) = add;
-let (x - y) = minus;
-let (x * y) = multiply;
-let (x / y) = divide;
+let firstNumber;
+let secondNumber;
+let step = 0;
+let operation;
+let result = 0;
+let finishedCalculation = false;
 
-if (num1 + num2);
+let numArray = [];
+let secondNumArray = [];
+let display = document.getElementById('display');
 
-const number = prompt("Please input some number.");
-  if (!number) {
-    pleaseTryAgain();
-  } else {
-    myHeading.textContent = `answer, ${number}`;
+function getNumber(num) {
+  if (finishedCalculation) {
+    resetCalculaton();
   }
+  if (step <= 1) {
+    numArray.push(num); //Everytime add in number can be push
+    firstNumber = Number(numArray.join(''));//merge into one string
+    display.value = firstNumber;
+    step = 1;
+  } else {
+    secondNumArray.push(num);
+    secondNumber = Number(secondNumArray.join(''));
+    display.value = secondNumber;
+  }
+}
 
+function getOperator(operator) {
+  if (step != 1 || operation) {
+    console.log("You can't change you operator for now.")
+    return;
+  }
+  step = 2;
+  operation = operator;
+}
+
+function resetCalculaton() {
+  finishedCalculation = false;
+  display.value = "";
+  firstNumber = null;
+  secondNumber = null;
+  step = 0;
+  operation = null;
+  result = 0;
+  numArray = [];
+  secondNumArray = [];
+}
+
+const calculateEquals = () => {
+  if (!firstNumber && !secondNumber) {
+    display.value = "Invalid input";
+    return;
+  }
+  if (operation === '+') {
+    result = firstNumber + secondNumber;
+    display.value = result;
+  } else if (operation === '-') {
+    result = firstNumber - secondNumber;
+    display.value = result;
+  } else if (operation === '*') {
+    result = firstNumber * secondNumber;
+    display.value = result;
+  } else if (operation === '/') {
+    result = firstNumber / secondNumber;
+    display.value = result;
+  }
+  finishedCalculation = true;
+}
